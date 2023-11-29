83cb876de15310c6839f2f1da5ecafa78784eb67
jang-namu <ironhee8005@gmail.com>
2023-11-30 01:05:58 +0900

    test: UserRepositoryTest - @TestContainers 추가 DataIntegrityViolationException으로 빌드 실패 이슈 해결 이유는??

--- src/test/java/com/example/todo/user/UserRepositoryTest.java
+++ src/test/java/com/example/todo/user/UserRepositoryTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-//@Testcontainers
+@Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
